dee832544132172a3af45b9385a663fcb067602e
cniscoding <asdfa.com>
2023-04-16 13:11:36 -0700

    add nav with placeholders

---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Signup() {
+  return <h1>registration</h1>
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=REACT_APP_GOOGLE_MAPS_API_KEY&callback=initMap&libraries=places"></script>
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
