c845bb5319e1f78070ad93fd2920f1e482220ac5
cniscoding <asdfa.com>
2023-04-17 10:54:46 -0700

    load json from DB from server app.js. need to refactor. currently working on port server port

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,11 +2,18 @@
 const { Pool } = require('pg');
 
 const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: 'localhost',
+  user: 'development',
+  database: 'remotetogether',
+  password: 'development',
+  port: 5432,
+  
+
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME
 };
 
 const db = new Pool(dbParams);
